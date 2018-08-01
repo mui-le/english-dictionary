bfcab1f9fb615d6d427aed62489eaa2d3e8f8eb3
legolas-le <ngonthapvinhhang@gmail.com>
2018-08-01 22:14:28 +0700

    [update] 0801

---
+++ daily_20180801.py
@@ -0,0 +1,107 @@
+my_word_list = {
+    "opposite": {
+        'explain': "if one thing or person is opposite another, they are facing each other",
+        'examples': [
+            "The people sitting opposite us looked very familiar.",
+            "on the wall opposite the door"
+        ],
+    },
+    "omission": {
+        'explain': "when you do not include or do not do something",
+        'examples': [
+            "The omission of her name was not deliberate act",
+            "His omisison from the team."
+        ],
+    },
+    "deliberate": {
+        'explain': "intended or planed",
+        'examples': [
+            "a deliberate attempt to humiliate her"
+        ],
+    },
+    "humiliate": {
+        'explain': "to make someone feel ashamed or stupid, especially when other people in present",
+        'examples': [
+            "Her boss humiliate her in front of all her colleague",
+        ],
+    },
+    "colleague": {
+        'explain': "someone you work with",
+        'examples': [
+            "a colleague of mine from the bank",
+        ],
+    },
+    "embarrass": {
+        'explain': "To make someone feel ashamed, nervous, or uncomfortable especially in front of other people",
+        'examples': [
+            "he didn't want to embarrass her by asking questions"
+        ],
+    },
+    "nervous": {
+        'explain': "worried, frightened about something, unable to relax",
+        'examples': [
+            "She was so nervous about her exams that she shouldn't sleep",
+        ],
+    },
+    "intend": {
+        'explain': "to have something in your mind as a plan or purpose",
+        'examples': [
+            "I intend to spend the night there.",
+            "I didn't intend her to see paiting until it was finished",
+            "I never intented things to turn out the way they did."
+        ],
+    },
+    "ashamed": {
+        'explain': "feeling very sorry and embarrassed because of something you have done",
+        "examples": [
+            "I felt ashamed of the things i'd said to him.",
+            "I'm ashamed to admit that I've never read any of his books.",
+            "she felt ashamed that she had missed her sister's wedding."
+        ],
+    },
+
+    "admit": {
+        'explain': "to agree unwillingly that something is true or someone else is right ",
+        "examples": [
+            "you may not like her, but you have to admit that she's good at her job",
+            "Paul admited to me that he sometimes feels jealous of my friendship with T",
+        ],
+    },
+    'unwillingly': {
+        'explain': "not wanting to do something and refusing to do it.",
+        'examples': [
+
+        ],
+    },
+    "evoke": {
+        'explain': "to produce a strong feeling or memory in someone",
+        'examples': [
+            "The photographs evoked strong memories of our holidays in France",
+            "",
+        ],
+    },
+    "tie": {
+        'explain': "",
+        'examples': [
+            "",
+        ],
+    },
+    "bind": {
+        'explain': "",
+        'examples': [
+            "",
+        ],
+    },
+    "suitcase": {
+        'explain': "",
+        'examples': [
+
+        ],
+    },
+    "twist": {
+        'explain': "",
+        'examples': [
+            "",
+        ],
+    },
+}

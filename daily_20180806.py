45e48356b124234171643ea580a7ca73e124a774
z-vanmui-le <z.vanmui.le@gree.net>
2018-08-14 09:32:17 +0700

    update

---
+++ daily_20180806.py
@@ -0,0 +1,9 @@
+my_work_list = {
+    "discipline": {
+        "explain": "a way of tranining someone so that they learn to control their behaviour and obey rules",
+        "examples": [
+            ""
+        ],
+    },
+    ""
+}

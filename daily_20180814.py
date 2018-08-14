3ab8b3c9e4dbf34860eb953a0ec1e2b5e9411415
z-vanmui-le <z.vanmui.le@gree.net>
2018-08-14 09:44:09 +0700

    daily_20180814

---
+++ daily_20180814.py
@@ -0,0 +1,8 @@
+my_work_list = {
+    "impediment": {
+        'explain': "a physical problem that makes speaking, hearing, or moving difficult",
+        'examples': [
+            "a speech impediment"
+        ],
+    },
+}

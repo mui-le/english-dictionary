881543ba1cf47494ff432ab4ac8618c6cfee02aa
z-vanmui-le <z.vanmui.le@gree.net>
2018-08-14 09:50:35 +0700

    update merge br

---
+++ daily_20180814.py
@@ -0,0 +1,8 @@
+my_work_list = {
+    'potential': {
+        'explain': "likely to develop into a particular type of person or thing in the future",
+        'example': [
+            'new ways of attracting potential customers'
+        ],
+    }
+}

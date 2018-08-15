1967e25bd19825f8ae4d636b7cbcbae91f662ada
z-vanmui-le <z.vanmui.le@gree.net>
2018-08-15 17:13:10 +0700

    update 2018 0815

---
+++ daily_20180815.py
@@ -0,0 +1,70 @@
+my_word_list = {
+    'Historycal': {
+        'explain': "relating to the part",
+        'examples': [
+            "places of historical interest",
+            "It's important to look at the novel in its historical context"
+        ],
+    },
+    "Perception": {
+        "explain": "the way you think about somethikng and your idea of what it's like",
+        "examples": [
+            "children's perception of the world",
+            "the public percention of the government's performance"
+        ],
+    },
+    "pretty": {
+        "explain": "fairly or more than a little",
+        "examples": [
+            "I'm pretty sure he'll say yes.",
+            "She still looks pretty miserable"
+        ],
+    },
+    "miserable": {
+        "explain": "extremely unhappy, for example because you feel lonely, cold, or badly trated",
+        "examples": [
+            "I have been so miserable since pat left me.",
+            "I spent the weekend feeling miserable"
+        ],
+    },
+    "incredible": {
+        "explain": "extremely good, large, or great",
+        "examples": [
+            "the view is just incredible",
+            "there was blood everywhere and the pain was incredible"
+        ],
+    },
+    "disruptive": {
+        "explain": "causing problem and preventing something from continuing in it's usual way",
+        "examples": [
+            "Night work can be very disruptive to home life",
+            "Mike's parents thought I was a disruptive influence",
+            "ways to handle disruptive pupils"
+        ],
+    },
+    "innovation": {
+        "explain": "a new idea, method, or invention",
+        "examples": [
+            "recent innovations in english teaching",
+        ],
+    },
+    "invention": {
+        "explain": "a userful machine, tool, instrument etc that has been invented",
+        "examples": [
+            "The diswasher is a wonderful invention"
+        ],
+    },
+    "instrument": {
+        "explain": [
+            "a small tool used in work such as science or medicine",
+            "an object used for producing music, such as a piano or violin"
+        ],
+        "examples": [
+            "electronice instrument"
+        ],
+    },
+    "surgical": {
+        "explain": "relating to or used for medical operations",
+        "examples": "surgical techniques",
+    }
+}

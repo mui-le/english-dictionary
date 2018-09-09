6bafb09331c2dfb2c04397e9f8e3efc10aba4211
legolas-le <ngonthapvinhhang@gmail.com>
2018-09-09 19:16:34 +0700

    [update] commit to make 2018 09 09

---
+++ daily_20180901.py
@@ -0,0 +1,57 @@
+my_word_list = {
+    "rotate": {
+        "explain": "to turn with a circular movement around a central point or to make somthing do this"
+        "examples": [
+            "the earth rotates on it axis once every 24 hours",
+            "rotate the pan halfway through the baking time"
+        ],
+
+    },
+    "circular": {
+
+    },
+    "conceal": {
+        "explain": "To hide something carefully"
+        "examples": [
+            "the shadows concealed her as she crept up to the house",
+            "the path was concealed by long grass"
+        ],
+    },
+    "movement": {
+        "explain": "",
+        "examples": [
+            ""
+        ]
+    },
+    "belief": {
+        "explain": "the feeling the something is definitely true or definitely exists",
+        "examples": [
+            "a strong belief in God"
+        ]
+    },
+    "definitely": {
+        "explain": "without any doubt",
+        "examples": [
+            "the hotel fitness centre is definitely worth a visit "
+        ]
+    },
+    "doubt": {
+        "explain": "",
+        "examples": [
+
+        ],
+    },
+    "circular": {
+        "explain": "",
+        "examples": [
+            ""
+        ]
+    },
+    "sincere": {
+        "explain": "",
+        "examples": [
+
+        ],
+    }
+
+}
---
+++ daily_20180903.py
@@ -0,0 +1,46 @@
+talk about this once also
+Procrastinate
+procrastinate
+rotate
+the shadow
+grass
+crept
+belief
+doubt
+definitely
+shape
+movement
+destruction
+sincere
+statement
+fitness
+compatible
+central
+centre 
+axis
+halfway
+the pan
+the baking time 
+baking 
+retrieves
+consequence
+behalf 
+suppose 
+obtain 
+inject 
+diverse
+arch
+the shadows concealed her as she crept up to the house
+the path was concealed by long grass 
+a concealed weapon
+to turn with a circular movement
+the civil rights movement of the 1960s
+her sin
+her sincere belief that her brother was not the murderer
+the hotel fitness centre is definitely worth a visit
+same it sucks
+could not delete it and covered home page.
+coverted
+to turn with a circular movement around 
+the earth rotates on it axis once 24 hours
+rotate the pan halfway through the baking time
